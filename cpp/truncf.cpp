4da97364265960853ccbe769743c7dd72842a6ad
ckjbgames <ckjbgames@gmail.com>
2017-05-16 10:36:32 -0400

    Add files via upload

---
+++ cpp/truncf.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include<string>
+void err() {
+	std::cerr << "There was an error with file trunctuation.\n" <<
+		"Sorry about that.\n";
+	std::exit(1);
+}
+size_t fsize(std::fstream& f) {
+	size_t s;
+	f.seekg(0,f.end);
+	s = f.tellg();
+	f.seekg(0,f.beg);
+	return s;
+}
+void truncf(std::fstream& f, int n, const char* name) {
+	char* buf = new char[fsize(f)];
+	f.read(buf,fsize(f));
+	f.seekg(0,f.beg);
+	f.close();
+	std::ofstream g(name,std::ofstream::trunc);
+	g.write(buf,n);
+}
+int main(int argc, char* argv[]) {
+	if(argc < 3) {
+		std::cerr << "You didn't give enough parameters.\n";
+		std::exit(1);
+	}
+	std::fstream f(argv[1],std::fstream::in|std::fstream::out);
+	int n = atoi(argv[2]);
+	if((n==0)||!f) { 
+		std::cerr << "Your argument is invalid.\n";
+		std::exit(1);
+	}
+	try {
+		truncf(f,n,argv[1]);
+	} catch(std::fstream::failure fail) {
+		err();
+	}
+	return 0;
+}
